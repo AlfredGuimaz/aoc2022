21b70bf45ef18edbaa9d7eedac1212a4ff0e04b2
Alfred <alfredguimaz@pgatourhq.com>
2023-02-27 21:03:24 -0500

    day 1 quick sort impl

--- src/Day01.kt
+++ src/Day01.kt
@@ -21,5 +21,21 @@ fun main() {
         return input.size
     }
 
+
     // Part 2 you can use a Sorted set to be more algorithmically efficient instead of using libs
+//quick sort
+    fun List<List<Int>>.topNElves(n: Int): Int {
+        fun findTopN(n: Int, element: List<Int>): List<Int>{
+            if (element.size ==n) return element
+            val x = element.random()
+            val small = element.filter{ it < x}
+            val equal = element.filter { it == x }
+            val big = element.filter { it > x }
+            if (big.size >= n) return findTopN(n, big)
+            if (equal.size + big.size >= n) return (equal + big).takeLast(n)
+            return findTopN(n - nqual.size - big.size, small)  + equal + big
+        }
+        return findTopN(n, this.map{it.sum()}).sum()
+
+
 }
