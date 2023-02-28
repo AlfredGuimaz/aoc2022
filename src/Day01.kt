d961592c94fbfc229e8d63fec704bc265e035204
Alfred <alfredguimaz@pgatourhq.com>
2023-02-27 20:50:15 -0500

    day 1 commit

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,25 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    fun parseInput(input: String){
+        val data = input.split("\n\n"). map {elf ->
+            elf.lines().map {it.toInt()}
+        }
+    }
+    fun List<List<Int>>.topNElves(n: Int): Int{
+        return map { it .sum() }
+                .sortedDescending()
+                .take(n)
+                .sum()
+    }
+
+    fun part1(input: String): Int {
+        val data = parseInput(input)
+        return data.topNSelves
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    // Part 2 you can use a Sorted set to be more algorithmically efficient instead of using libs
 }
